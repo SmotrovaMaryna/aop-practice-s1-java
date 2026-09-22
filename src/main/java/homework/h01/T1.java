f060f79319c0ab6de57cfb74cef4746fda20b494
Smotrova Maryna <smotrovam96@gmail.com>
2026-09-22 12:17:31 +0300

    added T1

--- src/main/java/homework/h01/T1.java
+++ src/main/java/homework/h01/T1.java
@@ -3,3 +3,7 @@ package homework.h01;
 // base
 // https://leetcode.com/problems/palindrome-number/
 public class T1 {}
+    public int smallestEvenMultiple(int n) {
+        return (n % 2 == 0) ? n : n * 2;
+    }
+}
