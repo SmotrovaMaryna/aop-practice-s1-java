f5056ddf70898990379ae0b5d1e9f831717145ae
Smotrova Maryna <smotrovam96@gmail.com>
2026-09-22 12:29:48 +0300

    added T2

--- src/main/java/homework/h01/T2.java
+++ src/main/java/homework/h01/T2.java
@@ -2,4 +2,8 @@ package homework.h01;
 
 // advanced
 // https://leetcode.com/problems/rectangle-area/
-public class T2 {}
+public class T2 {
+    public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+}
